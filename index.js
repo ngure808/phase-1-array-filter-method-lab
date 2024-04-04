b63bdb32c0aa9cc5ef3a0598f60feb6d4eba1cc4
ngure808 <mwangingure808@gmail.com>
2024-04-04 14:37:08 +0300

    filter practice

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(drivers, string){
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+} 
+console.log(findMatching(drivers, "bobby"))
+
+function fuzzyMatch(drivers, character){
+    return drivers.filter(word => word.charAt(0).toLowerCase() === character.charAt(0).toLowerCase())
+}
+console.log(fuzzyMatch (drivers, "S"))
+
+function matchName(drivers, driverName){
+    return drivers.filter(driver => driver.name.toLowerCase()[0] === driverName.toLowerCase()[0])
+}
